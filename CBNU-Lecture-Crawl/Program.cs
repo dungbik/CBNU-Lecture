836573b31a43a4fa89157e3977bf0207096e6ab6
dungbik <q1w2e30630@gmail.com>
2021-01-27 04:45:09 +0900

    개설강좌 크롤링 프로젝트 생성

---
+++ CBNU-Lecture-Crawl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CBNU_Lecture_Crawl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
